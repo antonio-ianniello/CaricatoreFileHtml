f40de4e1521f03ea63cd017b749a0c3f113215fa
anton <anton@MSI>
2020-04-19 21:53:07 +0100

    INIZIO PROGETTO MAVEN CUCUMBER

--- springBootwebAppdemo/src/main/java/it/uniroma3/springBootwebAppdemo/SpringBootwebAppdemoApplication.java
+++ springBootwebAppdemo/src/main/java/it/uniroma3/springBootwebAppdemo/SpringBootwebAppdemoApplication.java
@@ -4,17 +4,24 @@ import java.io.File;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import it.uniroma3.springBootwebAppdemo.controller.AppController;
 
 @SpringBootApplication
 @ComponentScan({"it.uniroma3.springBootwebAppdemo","it.uniroma3.springBootwebAppdemo.controller"})
-public class SpringBootwebAppdemoApplication {
+public class SpringBootwebAppdemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		new File(AppController.uploadDirectory).mkdir();
 		SpringApplication.run(SpringBootwebAppdemoApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootwebAppdemoApplication.class);
 	}
 
 }
